de38f6b88df0b0a4d21065a0e7b2dc3925028361
Sourav0809 <spathak7431@gmail.com>
2023-05-29 00:48:48 +0530

    Edit Added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const mainForm = document.querySelector("#main_form")
 const list = document.querySelector(".items")
-
+let BooknowButton = document.querySelector('.submit-btn')
 
 const name = document.querySelector("#person_name")
 const email = document.querySelector("#person_email")
@@ -12,14 +12,14 @@ const phone = document.querySelector("#person_phone")
 
 
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/08ee7597555341bcbc444aa444c3ea3a/BookedAppointments")
+    axios.get("https://crudcrud.com/api/471e6a0ca76546b1af4731d2d1d4e6d7/BookedAppointments")
         .then((res) => {
             console.log(res)
             for (let i = 0; i < res.data.length; i++) {
                 if ((list.firstElementChild.className == "list_heading")) {
                     list.removeChild(list.firstElementChild)
                 }
-                list.innerHTML += ` <li class='listitems'><span class="idtoken">${res.data[i]._id}</span><span contenteditable = 'true'>${res.data[i].person_Name}</span> <span>${res.data[i].person_Email}</span> <span>${res.data[i].person_Phone}</span><button class="edit-btn">Edit</button><button class='listitems_btn'>X</button></li> `
+                list.innerHTML += ` <li class='listitems'><span class="idtoken">${res.data[i]._id}</span><span>${res.data[i].person_Name}</span> <span>${res.data[i].person_Email}</span> <span>${res.data[i].person_Phone}</span><button class="edit-btn">Edit</button><button class='listitems_btn'>X</button></li> `
             }
         })
         .catch((err) => console.log(err))
@@ -34,9 +34,10 @@ window.addEventListener("DOMContentLoaded", () => {
 /*                     Function to Add items into Server                      */
 /* -------------------------------------------------------------------------- */
 
-mainForm.addEventListener("submit", additem)
+BooknowButton.addEventListener("click", additem)
 async function additem(e) {
     e.preventDefault()
+    console.log("abc")
     try {
         let itemObj = {
             person_Name: name.value,
@@ -44,20 +45,42 @@ async function additem(e) {
             person_Phone: phone.value
 
         }
-        //creating destruction to get the actual data from the obj
-        const { data } = await axios.post('https://crudcrud.com/api/08ee7597555341bcbc444aa444c3ea3a/BookedAppointments', itemObj)
-        if ((list.firstElementChild.className == "list_heading")) {
-            list.removeChild(list.firstElementChild)
+        if (e.target.textContent === "Submit Now") {
+            //creating destruction to get the actual data from the obj
+            const { data } = await axios.post('https://crudcrud.com/api/471e6a0ca76546b1af4731d2d1d4e6d7/BookedAppointments', itemObj)
+            if ((list.firstElementChild.className == "list_heading")) {
+                list.removeChild(list.firstElementChild)
+            }
+            // also adding those item into web page
+            let enteredName = name.value
+            let enteredEmail = email.value
+            let enteredPhone = phone.value
+            list.innerHTML += ` <li class='listitems'><span class="idtoken">${data._id}</span><span>${enteredName}</span> <span>${enteredEmail}</span> <span>${enteredPhone}</span><button class="edit-btn">Edit</button><button class='listitems_btn'>X</button></li> `
+            // making the input box empty
+            name.value = ""
+            email.value = ""
+            phone.value = ""
+
         }
-        // also adding those item into web page
-        let enteredName = name.value
-        let enteredEmail = email.value
-        let enteredPhone = phone.value
-        list.innerHTML += ` <li class='listitems'><span class="idtoken">${data._id}</span><span contenteditable = 'true'>${enteredName}</span> <span>${enteredEmail}</span> <span>${enteredPhone}</span><button class="edit-btn">Edit</button><button class='listitems_btn'>X</button></li> `
-        // making the input box empty
-        name.value = ""
-        email.value = ""
-        phone.value = ""
+        else {
+            const id = BooknowButton.id
+            const { data } = await axios.put(`https://crudcrud.com/api/471e6a0ca76546b1af4731d2d1d4e6d7/BookedAppointments/${id}`, itemObj)
+            console.log(data);
+            if ((list.firstElementChild.className == "list_heading")) {
+                list.removeChild(list.firstElementChild)
+            }
+            // also adding those item into web page
+            let enteredName = name.value
+            let enteredEmail = email.value
+            let enteredPhone = phone.value
+            list.innerHTML += ` <li class='listitems'><span class="idtoken">${id}</span><span>${enteredName}</span> <span>${enteredEmail}</span> <span>${enteredPhone}</span><button class="edit-btn">Edit</button><button class='listitems_btn'>X</button></li> `
+            // making the input box empty
+            name.value = ""
+            email.value = ""
+            phone.value = ""
+            BooknowButton.textContent = "Submit Now"
+            BooknowButton.id = ""
+        }
     } catch (error) {
         console.log(error);
     }
@@ -73,16 +96,16 @@ async function additem(e) {
 list.addEventListener("click", deleteItem)
 async function deleteItem(e) {
     try {
-        if (e.target.classList.contains("listitems_btn")) {
-            if (confirm("Are You Sure ? ")) {
-                const li = e.target.parentElement
-                const idToken = li.firstElementChild.innerText
-                await axios.delete(`https://crudcrud.com/api/08ee7597555341bcbc444aa444c3ea3a/BookedAppointments/${idToken}`)
 
-                list.removeChild(li)
+        if (confirm("Are You Sure ? ")) {
+            const li = e.target.parentElement
+            const idToken = li.firstElementChild.innerText
+            await axios.delete(`https://crudcrud.com/api/471e6a0ca76546b1af4731d2d1d4e6d7/${idToken}`)
 
-            }
+            list.removeChild(li)
+
         }
+
         if (list.children.length == 0) {
             list.innerHTML += "<h1 class='list_heading'>No Appointment Booked !! </h1>"
         }
@@ -126,24 +149,13 @@ function filtervalues() {
 list.addEventListener("click", edit)
 
 function edit(e) {
-    if (e.target.classList.contains("edit-btn")) {
-        if (e.target.innerText == "Edit") {
-            e.target.innerText = "Save"
-            e.target.parentElement.children[0].setAttribute("contenteditable", "true")
 
-        }
-        else {
-            e.target.innerText = "Edit"
-            e.target.parentElement.children[0].setAttribute("contenteditable", "false")
-            let editObj = {
-                person_Name: e.target.parentElement.children[0].textContent,
-                person_Email: e.target.parentElement.children[1].textContent,
-                person_Phone: e.target.parentElement.children[2].textContent
+    name.value = e.target.parentElement.children[1].textContent
+    email.value = e.target.parentElement.children[2].textContent
+    phone.value = e.target.parentElement.children[3].textContent
 
-            }
+    BooknowButton.textContent = "Save Now"
+    BooknowButton.id = e.target.parentElement.children[0].textContent
+    e.target.parentElement.remove()
 
-            localStorage.setItem(e.target.parentElement.children[1].textContent, JSON.stringify(editObj))
-
-        }
-    }
 }
